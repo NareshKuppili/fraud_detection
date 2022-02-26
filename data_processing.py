0cf446cb083c477392c34db8d29544a516a1c751
NareshKuppili <nareshkuppili90@gmail.com>
2022-02-26 09:18:44 +0530

    added mehods

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,20 @@
 print("Adding all the data processing code related to prod detection")
 
 
+
 a = 5
 b = 7
+
 
 def sum(a, b):
     return a+b
 
 
-print(f'sum of {a} amd {b} is {sum(a,b)}')
+print(sum(a,b))
+
+def mul(a, b):
+    return a*b
+
+
+
+print(mul(a,b))
